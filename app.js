fedb1ef46ddbec335cff7c93d59f5101e73d9056
Bri <brigitta.rucz@yahoo.com>
2020-09-18 17:11:02 +0200

    structure

--- app.js
+++ app.js
@@ -4,14 +4,20 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const routes = require('./routes/routes');
 const db = require('./util/database'); // The pool itself
+const { LocalStorage } = require('node-localstorage');
+localStorage = new LocalStorage('./scratch');
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes);
+
+app.use('/logout', (req,res,next) => {
+    localStorage.clear();
+    return res.redirect('/authenticate');
+})
 
 app.use((req,res,next) => {
     res.status(404).send('<h1>Page not found</h1>');
@@ -22,11 +28,5 @@ db.execute('SELECT * FROM users').then(result =>{
 }).catch(err => {
     console.log(err);
 });
-
-// db.end(); when we shut the app
-
-// app.use((req,res,next) => {
-//     res.send('Hello '+ dir);
-// })
 
 app.listen(3000);
--- controllers/events.js
+++ controllers/events.js
@@ -3,40 +3,63 @@ const { LocalStorage } = require('node-localstorage');
 localStorage = new LocalStorage('./scratch');
 const Event = require('../models/event');
 const dateFormatter = require('date-fns')
+const fetch = require('node-fetch');
 
-exports.getEvents = (req,res,next) => {
+exports.getEvents = async (req,res,next) => {
 
     Event.fetchEvents().then(resp => {
         var aEvents = resp[0];
 
-        aEvents.forEach(oEvent => {
+        async function processEvents() {
 
-            var finalDate = [];
+            for(const oEvent of aEvents) {
+                var finalDate = [];
 
-            if(oEvent.date.includes("-")) {
-                var aDates = oEvent.date.split("-");
-                var aDateBegin = aDates[0].split(".");
-                aDateBegin = dateFormatter.format(new Date(aDateBegin[2], (parseInt(aDateBegin[1]) - 1), aDateBegin[0]), 'PPP');
-                var aDateEnd = aDates[1].split(".");
-                aDateEnd = dateFormatter.format(new Date(aDateEnd[2], (parseInt(aDateEnd[1]) - 1), aDateEnd[0]), 'PPP');
-                finalDate.push(aDateBegin);
-                finalDate.push(aDateEnd);
-            } else {
-                var aDate = oEvent.date.split(".");
-                aDate = dateFormatter.format(new Date(aDate[2], (parseInt(aDate[1]) - 1), aDate[0]), 'PPP');
-                finalDate.push(aDate);
+                if(oEvent.date.includes("-")) {
+                    var aDates = oEvent.date.split("-");
+                    var aDateBegin = aDates[0].split(".");
+                    aDateBegin = dateFormatter.format(new Date(aDateBegin[2], (parseInt(aDateBegin[1]) - 1), aDateBegin[0]), 'PPP');
+                    var aDateEnd = aDates[1].split(".");
+                    aDateEnd = dateFormatter.format(new Date(aDateEnd[2], (parseInt(aDateEnd[1]) - 1), aDateEnd[0]), 'PPP');
+                    finalDate.push(aDateBegin);
+                    finalDate.push(aDateEnd);
+                } else {
+                    var aDate = oEvent.date.split(".");
+                    aDate = dateFormatter.format(new Date(aDate[2], (parseInt(aDate[1]) - 1), aDate[0]), 'PPP');
+                    finalDate.push(aDate);
+                }
+
+                oEvent.date = finalDate;
+
+                if(oEvent.attendance_price.charAt(0) === 'F') {
+                    var price = oEvent.attendance_price.slice(1, oEvent.attendance_price.length);
+                    price = 'From ' + price + 'DKK';
+                    oEvent.attendance_price = price;
+                } else if (oEvent.attendance_price.includes('NA')) {
+                    oEvent.attendance_price = 'Not available';
+                } else if (oEvent.attendance_price === '0') {
+                    oEvent.attendance_price = 'Free';
+                }
+        
+                // urlMetadata = await fetch('https://url-metadata.herokuapp.com/api/metadata?url=' + oEvent.event_link)
+                // .then(response => response.json())
+                // .then(data => { 
+                //     oEvent.image =  data.data.image; 
+                // }).catch(error => {
+                //     oEvent.image = 'images/event-placeholder.jpg';
+                // });
             }
 
-            oEvent.date = finalDate;
+            return aEvents;
+        };
 
-
-        })
-
-        res.render('events/homepage', {
-            pageTitle: 'Homepage',
-            events: aEvents,
-            sessionId: localStorage.getItem('sessionId')
-        })
+        processEvents().then(aEvents => {
+            res.render('events/homepage', {
+                pageTitle: 'Homepage',
+                events: aEvents,
+                sessionId: localStorage.getItem('sessionId')
+            })
+        });
     }).catch(error => {
         res.status(404);
             res.setHeader('Content-Type', 'application/json');
